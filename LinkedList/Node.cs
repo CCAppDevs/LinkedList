dad9cc006c2b5d2cd40c41a1090a8c56eae5bc48
Jesse Harlan <jesse.harlan@centralia.edu>
2024-10-24 13:54:34 -0700

    Add project files.

---
+++ LinkedList/Node.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedList
+{
+    public class Node
+    {
+        // knows (properties)
+        // what is its data (payload)
+        // whats next (next node)
+        string Directions { get; set; }
+
+        Node Next { get; set; }
+
+        // does (methods)
+        public Node()
+        {
+
+        }
+    }
+}
---
+++ LinkedList/Program.cs
@@ -0,0 +1,13 @@
+﻿using System.Linq.Expressions;
+
+namespace LinkedList
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Node test = new Node();
+
+        }
+    }
+}
